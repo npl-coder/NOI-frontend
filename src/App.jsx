2370db1f49beb28a0c7ade1d2627c2c663d6172b
Aashish Karki <078bct004.aashish@pcampus.edu.np>
2024-12-15 08:38:35 +0545

    Added Mission section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Footer from "./components/Footer/Footer"
 import GetInvolved from "./components/GetInvolved/GetInvolved"
 import Hero from "./components/Hero/Hero"
+import Mission from "./components/Mission/Mission"
 import Navbar from "./components/Navbar/Navbar"
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
      <Navbar />
      <Hero />
      <GetInvolved />
+     <Mission />
      <Footer />
     </>
   )
--- src/components/Mission/Mission.jsx
+++ src/components/Mission/Mission.jsx
@@ -1,12 +1,21 @@
-
-
+import './mission.css'
 const Mission = () => {
   return (
     <div className="mission">
-    <h2>Our Mission</h2>
-    <p>We&appos;re committed to inspiring young tech enthusiasts in Nepal by providing the resources and mentorship they need to thrive. Despite the obstacles in our competitive programming landscape, our goal is to ensure Nepal's participation in the International Olympiad in Informatics (IOI).Through this initiative, we aim to boost student engagement in competitive programming and empower young coders to represent Nepal on the global stage. Together, we can make a meaningful difference and create opportunities for the next generation of tech leaders.</p>
-  </div>
-  )
-}
+        <h2>Our Mission</h2>
+        <p>
+          We're committed to inspiring young tech enthusiasts in Nepal by
+          providing the resources and mentorship they need to thrive. Despite
+          the obstacles in our competitive programming landscape, our goal is to
+          ensure Nepal's participation in the International Olympiad in
+          Informatics (IOI).Through this initiative, we aim to boost student
+          engagement in competitive programming and empower young coders to
+          represent Nepal on the global stage. Together, we can make a
+          meaningful difference and create opportunities for the next generation
+          of tech leaders.
+        </p>
+    </div>
+  );
+};
 
-export default Mission
+export default Mission;
